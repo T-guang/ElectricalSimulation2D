af0b18824b1a9d8d852a7e767863f76739e49b44
L_chen_guang <1964905645@qq.com>
2026-06-30 15:20:59 +0800

    微调电机风扇旋转速度为720度/秒

--- Assets/Scripts/UI/VisualPrefab/MotorVisualController.cs
+++ Assets/Scripts/UI/VisualPrefab/MotorVisualController.cs
@@ -6,7 +6,7 @@ namespace ElectricalSim.UI.VisualPrefab
     public class MotorVisualController : MonoBehaviour
     {
         [SerializeField] private RectTransform fanPivot;
-        [SerializeField] private float rotationSpeed = -1080f;
+        [SerializeField] private float rotationSpeed = -720f;
 
         private CircuitComponent component;
 
