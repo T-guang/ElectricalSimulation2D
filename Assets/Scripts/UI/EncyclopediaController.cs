d78fd119984ef0ae861da311aacd25fa47023128
L_chen_guang <1964905645@qq.com>
2026-07-02 13:16:53 +0800

    修复百科卡片文字堆叠及侧边栏排版瑕疵

--- Assets/Scripts/UI/EncyclopediaController.cs
+++ Assets/Scripts/UI/EncyclopediaController.cs
@@ -165,11 +165,11 @@ namespace ElectricalSim.UI
             {
                 var category = categories[i];
                 var buttonRect = CreatePanel("Category_" + category, sidebar, new Color(0.96f, 0.98f, 1f, 1f));
-                SetRect(buttonRect, new Vector2(0f, 1f), new Vector2(1f, 1f), new Vector2(0.5f, 1f), new Vector2(14f, -72f - i * 48f), new Vector2(-28f, 38f));
+                SetRect(buttonRect, new Vector2(0f, 1f), new Vector2(1f, 1f), new Vector2(0.5f, 1f), new Vector2(0f, -72f - i * 48f), new Vector2(-32f, 38f));
                 var button = buttonRect.gameObject.AddComponent<Button>();
                 var label = CreateText("Text", buttonRect, category, 16, FontStyle.Normal, new Color(0.18f, 0.24f, 0.32f));
                 label.alignment = TextAnchor.MiddleLeft;
-                SetRect(label.rectTransform, Vector2.zero, Vector2.one, new Vector2(0.5f, 0.5f), new Vector2(14f, 0f), new Vector2(-20f, 0f));
+                SetRect(label.rectTransform, Vector2.zero, Vector2.one, new Vector2(0.5f, 0.5f), new Vector2(16f, 0f), new Vector2(-16f, 0f));
                 var captured = category;
                 button.onClick.AddListener(() =>
                 {
@@ -357,7 +357,7 @@ namespace ElectricalSim.UI
             infoArea.offsetMax = new Vector2(-16f, -18f);
 
             var infoLayout = infoArea.gameObject.AddComponent<VerticalLayoutGroup>();
-            infoLayout.childAlignment = TextAnchor.UpperLeft;
+            infoLayout.childAlignment = TextAnchor.MiddleLeft;
             infoLayout.childControlWidth = true;
             infoLayout.childControlHeight = true;
             infoLayout.childForceExpandWidth = true;
@@ -365,32 +365,24 @@ namespace ElectricalSim.UI
             infoLayout.spacing = 6f;
 
             var name = CreateText("NameText", infoArea, entry.DisplayName, 16, FontStyle.Bold, new Color(0.07f, 0.11f, 0.18f));
-            name.alignment = TextAnchor.UpperLeft;
+            name.alignment = TextAnchor.MiddleLeft;
             name.horizontalOverflow = HorizontalWrapMode.Wrap;
-            name.verticalOverflow = VerticalWrapMode.Truncate;
-            var nameLayout = name.gameObject.AddComponent<LayoutElement>();
-            nameLayout.preferredHeight = 42f;
+            name.verticalOverflow = VerticalWrapMode.Overflow;
 
             var category = CreateText("CategoryText", infoArea, entry.Category, 12, FontStyle.Normal, new Color(0.35f, 0.42f, 0.52f));
             category.alignment = TextAnchor.MiddleLeft;
             category.horizontalOverflow = HorizontalWrapMode.Wrap;
-            category.verticalOverflow = VerticalWrapMode.Truncate;
-            var catLayout = category.gameObject.AddComponent<LayoutElement>();
-            catLayout.preferredHeight = 16f;
+            category.verticalOverflow = VerticalWrapMode.Overflow;
 
             var param = CreateText("ParamText", infoArea, BuildBasicSummary(entry), 12, FontStyle.Normal, new Color(0.08f, 0.32f, 0.60f));
             param.alignment = TextAnchor.MiddleLeft;
             param.horizontalOverflow = HorizontalWrapMode.Wrap;
-            param.verticalOverflow = VerticalWrapMode.Truncate;
-            var paramLayout = param.gameObject.AddComponent<LayoutElement>();
-            paramLayout.preferredHeight = 16f;
+            param.verticalOverflow = VerticalWrapMode.Overflow;
 
             var terminals = CreateText("TerminalText", infoArea, BuildTerminalSummary(entry.Definition, entry.Terminals), 12, FontStyle.Normal, new Color(0.18f, 0.24f, 0.32f));
-            terminals.alignment = TextAnchor.UpperLeft;
+            terminals.alignment = TextAnchor.MiddleLeft;
             terminals.horizontalOverflow = HorizontalWrapMode.Wrap;
-            terminals.verticalOverflow = VerticalWrapMode.Truncate;
-            var termLayout = terminals.gameObject.AddComponent<LayoutElement>();
-            termLayout.preferredHeight = 32f;
+            terminals.verticalOverflow = VerticalWrapMode.Overflow;
 
             return card;
         }
